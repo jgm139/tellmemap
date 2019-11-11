412dd067d0245dee0e2362d0f4f4b11aafc2d5e1
Julia García Martínez <jgm139@alu.ua.es>
2019-11-11 11:27:58 +0100

    Vista para crear mensajes y conexión entre vistas

--- TellMeMap/TellMeMap/TableViewController.swift
+++ TellMeMap/TellMeMap/TableViewController.swift
@@ -20,6 +20,28 @@ class TableViewController: UITableViewController {
 
         self.myDataSource = DSTable()
         self.myTableView.dataSource = myDataSource
+        
+        let plusButton = UIButton(frame: CGRect(origin: CGPoint(x: self.view.frame.width - 100, y: self.view.frame.height - 100), size: CGSize(width: 60, height: 60)))
+        
+        let icon = UIImage(systemName: "plus.circle.fill")
+        plusButton.setImage(icon, for: .normal)
+        
+        plusButton.contentVerticalAlignment = .fill
+        plusButton.contentHorizontalAlignment = .fill
+        plusButton.imageEdgeInsets = UIEdgeInsets(top: 1, left: 1, bottom: 1, right: 1)
+        
+        plusButton.addTarget(self, action: #selector(actionAddSign), for: .touchUpInside)
+        
+        self.navigationController?.view.addSubview(plusButton)
+    }
+    
+    @objc func actionAddSign() {
+        // Safe Present
+        if let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "NewSignVC") as? ViewController {
+            vc.modalPresentationStyle = .fullScreen
+            vc.modalTransitionStyle = .flipHorizontal
+            present(vc, animated: true, completion: nil)
+        }
     }
 
 }
--- TellMeMap/TellMeMap/ViewController.swift
+++ TellMeMap/TellMeMap/ViewController.swift
@@ -8,13 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextViewDelegate {
+    
+    //MARK: Properties
+    @IBOutlet weak var newSignDescription: UITextView!
+    @IBOutlet weak var newSignTitle: UILabel!
+    
+    //MARK: Functions
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.newSignDescription.delegate = self
     }
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        self.newSignDescription.text = "Description"
+        self.newSignDescription.textColor = UIColor.lightGray
+    }
+    
+    @IBAction func goBack(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    //MARK: UITextViewDelegate functions
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if textView.textColor == UIColor.lightGray {
+            textView.text = nil
+            textView.textColor = UIColor.black
+        }
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if textView.text.isEmpty {
+            textView.text = "Description"
+            textView.textColor = UIColor.lightGray
+        }
+    }
+    
+    func textViewDidChange(_ textView: UITextView) {
+        self.newSignTitle.text = String(textView.text.split(separator: "\n")[0])
+    }
 
 }
 
