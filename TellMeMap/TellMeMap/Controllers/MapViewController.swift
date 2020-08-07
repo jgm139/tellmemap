20782a1e1efc1bda1ac56c0f1d13c64b1cc545c2
jgm139 <jgm139@alu.ua.es>
2020-08-07 18:18:37 +0200

    Anotaciones añadidas al mapa

--- TellMeMap/TellMeMap/Controllers/MapViewController.swift
+++ TellMeMap/TellMeMap/Controllers/MapViewController.swift
@@ -7,13 +7,35 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
+    
+    // MARK: - Properties
+    var ckManager = CloudKitManager()
+    
+    // MARK: - Outlets
+    @IBOutlet weak var mapView: MKMapView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        ckManager.getPlaces {
+            (finish) in
+            if finish {
+                self.ckManager.places.forEach {
+                    (item) in
+                    let artPin = ArtworkPin(title: item.name!, subtitle: item.message!, coordinate: item.location!)
+                    
+                    self.mapView.addAnnotation(artPin)
+                }
+            }
+        }
     }
 
 }
