e68fd2ba9a061dc3b4fe65237b240e1bc3550caf
jgm139 <jgm139@alu.ua.es>
2020-08-07 17:46:52 +0200

    Vista del mapa con su respectivo controlador

---
+++ TellMeMap/TellMeMap/Controllers/MapViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MapViewController.swift
+//  tellMeMap
+//
+//  Created by Julia García Martínez on 07/08/2020.
+//  Copyright © 2020 Julia García Martínez. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
